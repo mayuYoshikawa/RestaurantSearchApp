9afa16dad564a328da9db24b75dcee8baf155b93
mayu yoshikawa <m.yoshikawa25@gmail.com>
2022-11-16 09:48:01 +0900

    11月16日Top画面修正

--- app/src/main/java/com/example/restaurantsearchapp/MainActivity.kt
+++ app/src/main/java/com/example/restaurantsearchapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.restaurantsearchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.restaurantsearchapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_top)
+        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/example/restaurantsearchapp/fragment/TopFragment.kt
@@ -0,0 +1,42 @@
+package com.example.restaurantsearchapp.fragment
+
+import android.R
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
+import com.example.restaurantsearchapp.databinding.FragmentTopBinding
+
+class TopFragment : Fragment() {
+    private var _binding: FragmentTopBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTopBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
